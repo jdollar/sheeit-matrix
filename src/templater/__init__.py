3d346f35924b59423fa2eaafe66ff21632255c47
Joel Dollarhide <dollarhidejoel@gmail.com>
2023-08-30 21:13:23 -0400

    make toc on back

--- src/templater/__init__.py
+++ src/templater/__init__.py
@@ -3,10 +3,17 @@ import random
 from pyhtml2pdf import converter
 import os
 from pathlib import Path
-from typing import Optional
 import typer
 from jinja2 import Environment, FileSystemLoader
 from typing_extensions import Annotated
+
+def divide_chunks(l, n):
+    for i in range(0, len(l), n): 
+        chunk = l[i:i + n]
+        # pad the array with empty strings
+        # if we don't have enough
+        chunk = chunk + ([''] * (n - len(chunk)))
+        yield chunk
 
 def main(
     template: Annotated[
@@ -19,15 +26,27 @@ def main(
             resolve_path=True
         )
     ],
-    count: Annotated[int, typer.Option(min=0)],
-    code: Annotated[int, typer.Option(min=0, max=999)],
-    violation: Annotated[int, typer.Option(min=0, max=999)],
+    count: Annotated[
+        int,
+        typer.Option(
+            min=0,
+            help="Number of QR codes to generate",
+        )
+    ],
+    code: Annotated[
+        int,
+        typer.Option(min=0, max=999)
+    ],
+    violation: Annotated[
+        int,
+        typer.Option(min=0, max=999)
+    ],
     date: Annotated[datetime, typer.Option(formats=['%Y%m%d'])] = datetime.now(),
     cle: Annotated[int, typer.Option(min=0, max=999)] = 0,
 ):
     environment = Environment(loader=FileSystemLoader(template.cwd()))
-
-    template = environment.get_template(template.name)
+    environment.globals.update(divide_chunks=divide_chunks)
+    templateFile = environment.get_template(template.name)
 
     already_created = set()
     qr_codes = list()
@@ -35,7 +54,10 @@ def main(
         serial_number = ""
         while serial_number == "" or serial_number in already_created:
             rando = random.randrange(100000, 900000)
-            serial_number = f"{date.strftime('%Y%m%d')}{code}{violation}{cle}{rando}"
+            code_str = str(code).zfill(3)
+            violation_str = str(violation).zfill(3)
+            cle_str = str(cle).zfill(3)
+            serial_number = f"{date.strftime('%Y%m%d')}{code_str}{violation_str}{cle_str}{rando}"
 
         already_created.add(serial_number)
 
@@ -45,9 +67,8 @@ def main(
 
     output_html_file = "test.html"
     with open(output_html_file, mode="w", encoding="utf-8") as html_file:
-        content = template.render(qr_codes=qr_codes)
+        content = templateFile.render(qr_codes=qr_codes)
         html_file.write(content)
-        print(f"Wrote { output_html_file }")
 
     output_pdf_file = "test.pdf"
     converter.convert(f"file:///{os.path.abspath(output_html_file)}", output_pdf_file)
